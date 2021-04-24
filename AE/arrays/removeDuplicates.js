2cf58724e59dc21b6a11dad45a22efebe6fff7e7
giaedgington <gia.edgington@gmail.com>
2021-04-24 18:16:16 -0400

    Add source

--- AE/arrays/removeDuplicates.js
+++ AE/arrays/removeDuplicates.js
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
 var removeDuplicates = function(nums) { 
     for(let i = nums.length - 1; i >= 0; i--){
         if(nums[i] === val){
